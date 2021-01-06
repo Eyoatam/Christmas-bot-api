3a894a0879a9d5ac3468fb8ea1e13c7ed8352981
Eyoatam <eyoatamtamirat7@gmail.com>
2021-01-06 14:41:54 +0300

    ci(fix): fix ci issues

--- app.ts
+++ app.ts
@@ -7,16 +7,15 @@ import mongoose from "mongoose";
 import { Request, Response, Application, NextFunction } from "express";
 
 import messageRoutes from "./routes/MessageRoutes";
-import telegramBotRoutes from "./routes/telegramBotRoutes";
 
 dotenv.config();
 const app: Application = express();
 
 mongoose.connect(
-  "mongodb+srv://ChristmasBotAdmin:" +
-    process.env.MONGO_ATLAS_PASSWORD +
-    "@christmasbot.7ifob.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  { useUnifiedTopology: true, useNewUrlParser: true }
+	"mongodb+srv://ChristmasBotAdmin:" +
+		process.env.MONGO_ATLAS_PASSWORD +
+		"@christmasbot.7ifob.mongodb.net/<dbname>?retryWrites=true&w=majority",
+	{ useUnifiedTopology: true, useNewUrlParser: true }
 );
 
 app.use(cors());
@@ -25,25 +24,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/messages", messageRoutes);
-app.use("/telegram_message", telegramBotRoutes);
 
 app.use((req, res, next) => {
-  const error = new Error("Not Found");
-  res.status(404).json({
-    error: {
-      message: error.message,
-    },
-  });
-  error.message = "Not Found";
-  next(error);
+	const error = new Error("Not Found");
+	res.status(404).json({
+		error: {
+			message: error.message,
+		},
+	});
+	error.message = "Not Found";
+	next(error);
 });
 
 app.use((error: Object, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({
-    error: {
-      message: error,
-    },
-  });
-  console.log(error);
+	res.status(500).json({
+		error: {
+			message: error,
+		},
+	});
+	console.log(error);
 });
 export default app;
